a9f85a8f94dff1a93a76f000b0542b534db58e7f
98913 <98913@LAPTOP-JQR66TAF>
2025-05-29 17:25:29 +0330

    Create hub

---
+++ ChatApp/Hub/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hub;
+
+public class ChatHub:Microsoft.AspNetCore.SignalR.Hub
+{
+    public async Task SendToGroup(string group, string user, string message)
+    {
+        await Clients.Group(group).SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task JoinGroup(string group)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, group);
+    }
+
+    public async Task LeaveGroup(string group)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, group);
+    }
+
+    public async Task SendPrivate(string receiverConnectionId, string sender, string message)
+    {
+        await Clients.Client(receiverConnectionId).SendAsync("ReceivePrivate", sender, message);
+    }
+}
