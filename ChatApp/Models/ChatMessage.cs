08e68e06b31d4cf53b4e43ea1aff6da9014b3dcf
98913 <98913@LAPTOP-JQR66TAF>
2025-05-29 17:50:14 +0330

    Create Message Model

---
+++ ChatApp/Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace ChatApp.Models;
+
+public class ChatMessage
+{
+    public int Id { get; set; }
+    public string Sender { get; set; }
+    public string? Receiver { get; set; }
+    public string? Group { get; set; }
+    public string Text { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.Now;
+}
